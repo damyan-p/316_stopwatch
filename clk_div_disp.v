352bcedc56538768367c96af521496cc01e26206
damyan-p <dpavlovic@utexas.edu>
2021-11-22 11:11:08 -0600

    Update clk_div_disp.v
    added realtime simulation speed

--- clk_div_disp.v
+++ clk_div_disp.v
@@ -1,20 +1,20 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: EE 316 Digital Logic Design
+// Engineer: Damyan Pavlovic
 // 
 // Create Date: 10/22/2021 11:19:11 AM
-// Design Name: 
+// Design Name: stopwatch_main
 // Module Name: clk_div_disp
-// Project Name: 
-// Target Devices: 
+// Project Name: 316_stopwatch
+// Target Devices: Basys3 
 // Tool Versions: 
-// Description: 
+// Description: clock division module
 // 
 // Dependencies: 
 // 
 // Revision:
-// Revision 0.01 - File Created
+// Revision 0.02 - added realtime simulation speed
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
@@ -26,28 +26,38 @@ module clk_div_disp(
     output c_clk,
     output d_clk
     );
-    //reg [16:0] COUNT;   //  clock speed for final (1ms) verification
-    //reg [26:0] COUNT;     //  clock speed for realtime (1s) verification
-    reg [1:0] COUNT;  //  clock speed for simulation
+    //reg [16:0] COUNT;                 //  uncomment for clock speed for final (1ms) verification
+    reg [26:0] COUNT;             //  uncomment for 1s realtime counter speed
+    //reg [1:0] COUNT;                //  uncomment for simulation clock speed
     reg [1:0] D_COUNT;
     
-    //assign cnt_clk = (COUNT == 17'b11000011010100000); //  clock speed for final (1ms) verification
-    //assign cnt_clk = (COUNT == 27'b101111101011110000100000000); //  clock speed for realtime (1s) verification
-    assign d_clk = (COUNT[1]); //  clock speed for simulation
-    assign c_clk = (D_COUNT[1]);    
+
+    //assign d_clk = (COUNT[1]);                  //  simulation clock speed
+    //assign d_clk = (COUNT == 17'd100000);  //  uncomment for 1ms realtime counter speed
+    assign d_clk = (COUNT == 27'd100000000);  //  uncomment for 1s realtime counter speed
     
-    //assign an_clk = (COUNT == 17'b11000011010100000); //  clock speed for final (1ms) verification TODO 4x as fast
-    //assign an_clk = (COUNT == 27'b101111101011110000100000000); //  clock speed for realtime (1s) verification TODO 4x as fast
-    //assign an_clk = clk; //  clock speed for simulation    
+    assign c_clk = (D_COUNT[1]);    
     
     always @(posedge clk)
     begin
     if (reset)
         COUNT = 0;
-    else        //TODO
+        
+    /*  
+    //  uncomment for 1ms realtime counter speed, comment for simulation counter speed
+    else if(COUNT == 17'd100000)
+        COUNT = 0;
+    */
+    
+    //  uncomment for 1s realtime counter speed, comment for simulation counter speed
+    else if(COUNT == 27'd100000000)
+        COUNT = 0;
+    
+   
+    else
         COUNT = COUNT + 1;
     end
-
+    
     always @(posedge d_clk or posedge reset)
     begin
     if (reset)
