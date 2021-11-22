70b4369030b69d75585982ca2175e2236cb11a95
damyan-p <dpavlovic@utexas.edu>
2021-11-22 11:14:45 -0600

    Update tb_stopwatch.v
    added test modules for countdowns mode 2, mode 3

--- tb_stopwatch.v
+++ tb_stopwatch.v
@@ -1,21 +1,21 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: EE 316 Digital Logic Design
+// Engineer: Damyan Pavlovic
 // 
-// Create Date: 11/21/2021 03:13:36 PM
-// Design Name: 
+// Create Date: 10/22/2021 11:19:11 AM
+// Design Name: stopwatch_main
 // Module Name: tb_stopwatch
-// Project Name: 
-// Target Devices: 
+// Project Name: 316_stopwatch
+// Target Devices: Basys3, XC7A35TCPG236-1
 // Tool Versions: 
-// Description: 
+// Description: clock division module
 // 
 // Dependencies: 
 // 
 // Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
+// Revision 0.02 - added 
+// Additional Comments: added mode 2, 3 testing
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -55,9 +55,28 @@ sel = 2'b01;
 #100;
 R = 0;
 #1000;
+P = 1;  //  testing mode 1 count up from 99.00
+#5;
+P = 0;  //  testing mode 0 count up
+#200;
+R = 1;
+sel = 2'b10;
+#100;
+R = 0;
+#1000;
 P = 1;
 #5;
-P = 0;
+P = 0;  //  testing mode 2 count down from 99.99
+#200;
+R = 1;
+sel = 2'b11;
+load = 8'h01;
+#100;
+R = 0;
+#1000;
+P = 1;
+#5;
+P = 0;  //  testing mode 3 count down from 01.00
 end
 
 always
