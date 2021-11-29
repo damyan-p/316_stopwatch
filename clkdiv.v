8a636b5a273e02d9c202c1f19f6c94dfc2f75db5
HumzaSyed86 <55002289+HumzaSyed86@users.noreply.github.com>
2021-11-29 16:40:17 -0600

    Update clkdiv.v
    updated clock no flicker, pretty complete

--- clkdiv.v
+++ clkdiv.v
@@ -7,14 +7,14 @@ module clkdiv(
     output d_clk // display clk  
     );
     
-            reg [23:0] COUNT; // board display 
+            reg [18:0] COUNT; // board display 
             
-            reg [16:0] TIME_COUNT; 
+            reg [19:0] TIME_COUNT; 
        
            //reg [1:0] COUNT; 
             
-            assign d_clk = COUNT[23]; // real
-            assign c_clk = TIME_COUNT[16];// the stopwatch counter 
+            assign d_clk = COUNT[18]; // real
+            assign c_clk = (TIME_COUNT == 19'd1000000);// the stopwatch counter 
             
             
             //assign d_clk = COUNT[1]; // simulation 
@@ -24,15 +24,9 @@ module clkdiv(
                 if (reset) begin
                     COUNT <= 0; 
                     TIME_COUNT <= 0; 
-                end 
-            
-            
-//                if( TIME_COUNT >= 100000 ) begin
-//                    TIME_COUNT = 0; 
-                    
-//                end 
-            
-            
+                end
+                if( TIME_COUNT == 1000000 )
+                    TIME_COUNT = 0; 
                 else begin
                     COUNT <= COUNT + 1;
                     TIME_COUNT <= TIME_COUNT + 1;  
