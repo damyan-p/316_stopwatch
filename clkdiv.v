50b206d3a016939a790cc078e6316eb4e94546e3
HumzaSyed86 <55002289+HumzaSyed86@users.noreply.github.com>
2021-11-28 23:28:38 -0600

    trying something else

--- clkdiv.v
+++ clkdiv.v
@@ -9,12 +9,12 @@ module clkdiv(
     
             reg [23:0] COUNT; // board display 
             
-            reg [15:0] TIME_COUNT; 
+            reg [16:0] TIME_COUNT; 
        
            //reg [1:0] COUNT; 
             
             assign d_clk = COUNT[23]; // real
-            assign c_clk = TIME_COUNT[15];// the stopwatch counter 
+            assign c_clk = TIME_COUNT[16];// the stopwatch counter 
             
             
             //assign d_clk = COUNT[1]; // simulation 
@@ -22,13 +22,20 @@ module clkdiv(
             always @ (posedge clk)
                 begin 
                 if (reset) begin
-                    COUNT = 0; 
-                    //TIME_COUNT = 0; 
+                    COUNT <= 0; 
+                    TIME_COUNT <= 0; 
                 end 
             
+            
+//                if( TIME_COUNT >= 100000 ) begin
+//                    TIME_COUNT = 0; 
+                    
+//                end 
+            
+            
                 else begin
-                    COUNT = COUNT + 1;
-                    TIME_COUNT = TIME_COUNT + 1;  
+                    COUNT <= COUNT + 1;
+                    TIME_COUNT <= TIME_COUNT + 1;  
                 end
             
             end 
